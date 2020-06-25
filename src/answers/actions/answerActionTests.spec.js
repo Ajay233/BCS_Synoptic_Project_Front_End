37c41dd6007a0b54e57d874424f8e07755cffd90
Ajay <ajaymungur@hotmail.com>
2020-06-25 12:22:16 +0100

    Added remaining answer action tests

--- src/answers/actions/answerActionTests.spec.js
+++ src/answers/actions/answerActionTests.spec.js
@@ -15,7 +15,7 @@ const middleware = [thunk]
 const mockStore = configureMockStore(middleware)
 
 describe("getAnswers", () => {
-  it("should return an action", () => {
+  it("should return an action to set the answerList", () => {
     const store = mockStore({})
 
     const response = [ { id:1 },{ id:2 } ]
@@ -33,7 +33,7 @@ describe("getAnswers", () => {
 })
 
 describe("createAnswers", () => {
-  it("should return an action", () => {
+  it("should return an action to add an answer to the answerList", () => {
     const store = mockStore({})
 
     const response = [ { id:1 } ]
@@ -51,7 +51,7 @@ describe("createAnswers", () => {
 })
 
 describe("updateAnswers", () => {
-  it("should return an action", () => {
+  it("should return an action to updated an answer in the answerList", () => {
     const store = mockStore({})
 
     const response = [ { id:1 } ]
@@ -65,5 +65,48 @@ describe("updateAnswers", () => {
       mockAxios.mockResponse(response)
       expect(store.getActions()).toEqual(expectedAction)
     })
+  })
+})
+
+describe("deleteAnswer", () => {
+  it("should return an action to delete an answer fom the the answerList", () => {
+    const store = mockStore({})
+
+    const answer = [ { id:1 } ]
+
+    const expectedAction = {
+      type: "DELETE_ANSWER",
+      payload: [ { id:1 } ]
+    }
+
+    expect(deleteAnswer(answer)).toEqual(expectedAction)
+  })
+})
+
+describe("setCurrentAnswer", () => {
+  it("should return an action set the currentAnswer", () => {
+    const store = mockStore({})
+
+    const answer = { id:1 }
+
+    const expectedAction = {
+      type: "SET_CURRENT_ANSWER",
+      payload: { id:1 }
+    }
+
+    expect(setCurrentAnswer(answer)).toEqual(expectedAction)
+  })
+})
+
+describe("clearCurrentAnswer", () => {
+  it("should return an action to set the currentAnswer with an empty object", () => {
+    const store = mockStore({})
+
+    const expectedAction = {
+      type: "SET_CURRENT_ANSWER",
+      payload: {}
+    }
+
+    expect(clearCurrentAnswer()).toEqual(expectedAction)
   })
 })
